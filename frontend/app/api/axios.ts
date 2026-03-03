bab5d14e0fd962e61513bc22045a58863f9f2afe
mjbaker474 <mjbaker474@gmail.com>
2026-03-02 20:37:19 -0600

    Added interceptor to axios.ts to append JWT to requests

--- frontend/app/api/axios.ts
+++ frontend/app/api/axios.ts
@@ -1,7 +1,8 @@
 // this uses axios HTTP library
 
 import axios from "axios";
-import { Platform } from "react-native";
+import { Platform } from "react-native"
+import * as SecureStore from "expo-secure-store"
 
 // function that gets info of user's device
 const baseURL =
@@ -22,6 +23,20 @@ export const api = axios.create ({
         "Content-Type": "application/json",
     },
 });
+
+// Attach JWT to requests automatically
+api.interceptors.request.use(
+    async (config) => {
+        const token = await SecureStore.getItemAsync("token");
+
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+
+        return config;
+    },
+    (error) => Promise.reject(error)
+);
 
 // function to handle/simplify Axios error response
 // NOTE: the "pages/screen UI" will use this function to catch and display errors
