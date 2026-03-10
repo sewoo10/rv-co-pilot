85aba4ae392e73bee13ac83189d9d88fe9334acb
Eric Azevedo <azevedoe@oregonstate.edu>
2026-03-10 15:42:37 -0700

    added auto detect url for iOS instead of localhost

--- frontend/api/axios.ts
+++ frontend/api/axios.ts
@@ -2,17 +2,26 @@
 
 import axios from "axios";
 import { Platform } from "react-native"
+import Constants from "expo-constants";
 import * as SecureStore from "expo-secure-store"
 
-// function that gets info of user's device
-const baseURL =
-  Platform.OS === "ios"
-    ? "http://localhost:5000/api"
-    : Platform.OS === "android"
-    ? "http://10.0.2.2:5000/api"
-    : Platform .OS === "web"
-    ? "http://127.0.0.1:5000/api"
-    : "http://127.0.0.1:5000/api";
+const getAutoDetectedBaseURL = () => {
+    const expoHostUri = Constants.expoConfig?.hostUri;
+    if (expoHostUri) {
+        const host = expoHostUri.split(":")[0];
+        return `http://${host}:5000/api`;
+    }
+
+    if (Platform.OS === "web") {
+        const webHost = typeof window !== "undefined" ? window.location.hostname : "127.0.0.1";
+        return `http://${webHost}:5000/api`;
+    }
+
+    return Platform.OS === "android"
+        ? "http://10.0.2.2:5000/api"
+        : "http://127.0.0.1:5000/api";
+};
+const baseURL = getAutoDetectedBaseURL();
 
 
 // function that creates a reusable Axios client
